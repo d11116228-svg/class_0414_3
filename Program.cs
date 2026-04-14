3b65564f213cc04cdd5c367bdc22664fca34923e
d11116228-svg <d11116228@gs.takming.edu.tw>
2026-04-14 16:15:47 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Class_0414_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Caller c = new Caller();
+
+            c.CallGetName(new Person());
+
+            c.CallGetName(new Student());
+
+            c.CallGetName(new Teacher());
+        }
+    }
+}
